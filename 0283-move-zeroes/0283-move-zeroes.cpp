c09115839c04c3c419dca95183a13ab3f182611a
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-05-26 10:54:27 +0530

    Time: 0 ms (100%), Space: 24 MB (9.84%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zerotracker=0;
+        for(int nonzero=0;nonzero<nums.size();nonzero++){
+            if(nums[nonzero]!=0){
+                swap(nums[nonzero],nums[zerotracker]);
+            zerotracker++;
+            }
+                
+        }
+       
+    }
+};
