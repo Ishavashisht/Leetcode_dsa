36c10fcaa226379e0ce1251fe886008da6b078e0
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-05-31 10:00:32 +0530

    Time: 16 ms (86.3%), Space: 149 MB (85.38%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,33 @@
+class NumMatrix {
+public:
+    vector<vector<int>>prefixSum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        prefixSum.resize(m,vector<int>(n,0));
+     
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                prefixSum[i][j]=matrix[i][j];
+                if(i>0)prefixSum[i][j]+=prefixSum[i-1][j];
+                if(j>0)prefixSum[i][j]+=prefixSum[i][j-1];
+                if(i>0 && j>0)prefixSum[i][j]-=prefixSum[i-1][j-1];
+            }
+        }
+    }
+  //  prefix[i][j] = matrix[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+      int total = prefixSum[row2][col2];
+        if (row1 > 0) total -= prefixSum[row1-1][col2];
+        if (col1 > 0) total -= prefixSum[row2][col1-1];
+        if (row1 > 0 && col1 > 0) total += prefixSum[row1-1][col1-1];
+ return total;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
