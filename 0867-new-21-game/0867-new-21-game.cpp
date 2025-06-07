222a0af05e9d9d2bfffa5e7c6c4f4c7ed62e1cdf
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-06-07 11:47:22 +0530

    Time: 3 ms (67.03%), Space: 14.6 MB (5.13%) - LeetHub

---
+++ 0867-new-21-game/0867-new-21-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k==0 ||n>=k+maxPts-1)return 1.0;
+
+//dp->probabilty of winning if starting from x points 
+vector<double>dp(n+maxPts+1,0.0);
+for(int i=k;i<=n;i++){
+dp[i]=1.0;
+}
+
+//sliding window sum of proabilities
+double windowSum=n-k+1;
+for(int x=k-1;x>=0;--x){
+dp[x]=windowSum/maxPts;
+windowSum+=dp[x]-dp[x+maxPts];
+}
+return dp[0];
+
+    }
+};
