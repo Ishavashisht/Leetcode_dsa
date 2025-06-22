d5bf9df55c66d6419835f35f2db4a8020ca7dba8
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-06-22 18:48:17 +0530

    Time: 6 ms (95.46%), Space: 34.7 MB (78.74%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int m ,n;
-    vector<vector<int>>dir{{0,1},{0,-1},{1,0},{-1,0}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        m=mat.size();
-        n=mat[0].size();
-        vector<vector<int>>result(m,vector<int>(n,-1));
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>ans(m,vector<int>(n,INT_MAX));
         queue<pair<int,int>>q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    result[i][j]=0;
+                    ans[i][j]=0;
                     q.push({i,j});
                 }
             }
         }
-        while(!q.empty()){
-            pair<int,int>p=q.front();
+        vector<pair<int, int>>directions={{-1,0},{1,0},{0,-1},{0,1}};
+        while (!q.empty()) {
+            auto [i, j] = q.front();
             q.pop();
-            int i=p.first;
-            int j=p.second;
-            for(auto &dir:dir){
-                int new_i=i+dir[0];
-                int new_j=j+dir[1];
-                if(new_i>=0 && new_i<m && new_j>=0 && new_j < n &&result[new_i][new_j]==-1){
-                    result[new_i][new_j]=result[i][j]+1;
+
+            for (auto& dir : directions) {
+                int new_i = i + dir.first;
+                int new_j = j + dir.second;
+            if(new_i>=0 && new_i<m && new_j>=0 && new_j<n && ans[new_i][new_j]==INT_MAX){
+                
+                    ans[new_i][new_j]=ans[i][j]+1;
                     q.push({new_i,new_j});
                 }
             }
         }
-        return result;
+return ans;
     }
-    
 };
