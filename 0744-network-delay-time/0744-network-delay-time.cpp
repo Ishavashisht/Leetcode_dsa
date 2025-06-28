a8c6ce2a3955dedb3b76a2a422499da4a0e7d028
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-06-28 11:22:01 +0530

    Time: 83 ms (64.13%), Space: 44.3 MB (77.67%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-     unordered_map<int,vector<pair<int,int>>>adj;
-
-      for(auto&edge:times){
-        int u=edge[0];
-        int v=edge[1];
-        int w=edge[2];
-        adj[u].push_back({v,w});
-      } 
-      vector<int>dist(n+1,INT_MAX);
-    dist[k]=0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>pq;
-    pq.push({0,k});
-    while(!pq.empty()){
-        auto[currTime,u]=pq.top();
-        pq.pop();
-        if(currTime>dist[u])continue;
-        for(auto&[ngb,w] :adj[u]){
-            if(currTime+w<dist[ngb]){
-                dist[ngb]=currTime+w;
-                pq.push({dist[ngb],ngb});
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(vector<int>&vec:times){
+            int u=vec[0];
+            int v=vec[1];
+            int wt=vec[2];
+            adj[u].push_back({wt,v});
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>res(n+1,INT_MAX);
+        res[k]=0;
+        pq.push({0,k});
+        while(!pq.empty()){
+            int d=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            for(auto&vec:adj[node]){
+                int dist=vec.first;
+                int adjNode=vec.second;
+                if(d+dist<res[adjNode]){
+                    res[adjNode]=d+dist;
+                    pq.push({d+dist,adjNode});
+                }
             }
         }
-    }
-    int maxTime=0;
-    for(int i=1;i<=n;i++){
-        if(dist[i]==INT_MAX)return -1;
-        maxTime=max(maxTime,dist[i]);
-    }
-    return maxTime;
+        int ans=INT_MIN;
+        for(int i=1;i<=n;i++){
+            ans=max(ans,res[i]);
+        }
+        return ans==INT_MAX?-1:ans;
     }
 };
