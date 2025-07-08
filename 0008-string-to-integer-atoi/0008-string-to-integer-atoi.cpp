c956b57fa0bf20e70c2ea807c70840d5313c210d
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-07-08 11:54:30 +0530

    Time: 0 ms (100%), Space: 9.1 MB (56.54%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,32 +1,21 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0, sign = 1;
-        long res = 0; // Using long to handle overflow cases
-
-        // Trim leading spaces
+        int i=0,sign=1;
+        long res=0;
         while (i < s.size() && s[i] == ' ') i++;
-        if (i == s.size()) return 0;
-
-        // Check for sign
-        if (s[i] == '-') { sign = -1; i++; }
-        else if (s[i] == '+') i++;
-
-        // Process numerical characters
-        while (i < s.size() && isdigit(s[i])) {
-            res = res * 10 + (s[i] - '0');
-
-            // Handle overflow
-            if (sign * res > INT_MAX) return INT_MAX;
-            if (sign * res < INT_MIN) return INT_MIN;
-
+        if(i<s.size() && (s[i]=='+'||s[i]=='-')){
+            sign=(s[i]=='-')?-1:1;
+            i++;
+        }
+        while(i<s.size() && isdigit(s[i])){
+            res=res*10+(s[i]-'0');
+            if (sign * res >= INT_MAX) return INT_MAX;
+            if (sign * res <= INT_MIN) return INT_MIN;
             i++;
         }
 
-        return (int)(sign * res);
-    }
+        return sign * res;
+        }
+    
 };
