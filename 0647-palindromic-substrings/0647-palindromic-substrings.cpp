859cf43a2247157c21737414231a1cf575cb004e
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-07-10 09:32:42 +0530

    Time: 42 ms (22.96%), Space: 34.5 MB (20.59%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,87 +1,23 @@
 class Solution {
 public:
-//approch1 
-    bool check(string &s,int i,int j){
-        if(i>j){
-            return true;
-        }
-        if(s[i]==s[j]){
-            return check(s,i+1,j-1);
-        }
-        return false;
+    bool check(string &s, int i, int j,vector<vector<int>>&dp) {
+        if (i >= j) return true; // base case: single char or empty
+        if(dp[i][j]!=-1)return dp[i][j];
+        if (s[i] != s[j]) return false;
+        return dp[i][j]=check(s, i + 1, j - 1,dp); // move inward
     }
-    //approach2
-     bool checkMem(string &s,int i,int j,vector<vector<int>>&dp){
-        if(i>j){
-            return true;
-        }
-       if(dp[i][j]!=-1)return dp[i][j];
 
-        if(s[i]==s[j]){
-          dp[i][j]= checkMem(s,i+1,j-1,dp);
-          return dp[i][j];
-           
-        }
-        return dp[i][j]=false;
-    }
-    //approach3 
-    int solveTab(string s){
-        int n=s.length();
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
-        int count=0;
-        for(int L=1;L<=n;L++){
-        for(int i=0;i+L-1<n;i++){
-            int j=i+L-1;
-            if(i==j){
-                //one length
-                dp[i][j]=true;
+    int countSubstrings(string s) {
+        int n = s.length();
+        int count = 0;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (check(s, i, j,dp)) {
+                    count++;
+                }
             }
-          else if(i+1==j){
-            //length 2 
-            dp[i][j]=(s[i]==s[j]);
-          }
-          else {
-            dp[i][j]=(s[i]==s[j]&& dp[i+1][j-1]);
-          }
-          if(dp[i][j]==true){
-            count++;
-          }
-        }
         }
         return count;
-    }
-    //approach 4 
-    //using even and odd length 
-    int count;
-    void checkEO(string &s,int i,int j,int n){
-     
-        while(i>=0 && j<n && s[i]==s[j]){
-            count++;
-            i--;//left 
-            j++;
-        }
-    }
-    int countSubstrings(string s) {
-        int n=s.length();
-    //     vector<vector<int>>dp(n,vector<int>(n,-1));
-      count=0;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=i;j<n;j++){
-    //             //explore all the possible cases and then check them'
-    //             // if(check(s,i,j,dp)){
-    //                 if(checkMem(s,i,j,dp)){
-    //                 count++;
-    //             }
-    //         }
-    //     }
-    //     return count;
-    // }
-    // return solveTab(s);
-    //apprach 4 
-    for(int i=0;i<n;i++){
-        checkEO(s,i,i,n);
-        checkEO(s,i,i+1,n);
-    }
-    return count;
     }
 };
