91527150ff4d7b065386bc71630b28472767e221
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-07-24 15:35:01 +0530

    Time: 3 ms (47.89%), Space: 24 MB (23.18%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,22 +1,19 @@
 class NumArray {
 public:
-vector<int>prefix;
+    vector<int>prefix;
     NumArray(vector<int>& nums) {
         prefix.resize(nums.size());
         prefix[0]=nums[0];
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<nums.size();i++)
+        {
             prefix[i]=prefix[i-1]+nums[i];
         }
-     
     }
     
     int sumRange(int left, int right) {
         if(left==0)return prefix[right];
-      
-        else
-           return prefix[right]-prefix[left-1];
+return prefix[right]-prefix[left-1];
         
-       
     }
 };
 
