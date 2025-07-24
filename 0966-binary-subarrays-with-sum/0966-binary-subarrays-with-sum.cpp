4749f99e3b490236da52ca7586e3c3a5cf7015b7
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-07-24 18:02:16 +0530

    Time: 15 ms (34.16%), Space: 40 MB (9.89%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-       int n=nums.size();
-       vector<int>prefix(n);
-       prefix[0]=nums[0];
-       for(int i=1;i<n;i++){
-        prefix[i]=prefix[i-1]+nums[i];
-       } 
-       unordered_map<int,int>mp;
+      
        int count=0;
-       for(int j=0;j<n;j++)
-{
-if(prefix[j]==goal){
-    count++;
-}
-int val=prefix[j]-goal;
-if(mp.find(val)!=mp.end()){
-    count+=mp[val];
-}
-mp[prefix[j]]++;
-}    
-return count;
-}
+      vector<int>prefixSum(nums.size());
+      prefixSum[0]=nums[0];
+      for(int i=1;i<nums.size();i++){
+         prefixSum[i]=prefixSum[i-1]+nums[i];
+
+      }
+ unordered_map<int,int>mp;
+ for(int j=0;j< nums.size();j++){
+    if(prefixSum[j]==goal){
+        count++;
+    }
+    int val=prefixSum[j]-goal;
+    if(mp.find(val)!=mp.end()){
+        count+=mp[val];
+    }
+    mp[prefixSum[j]]++;
+ }
+ return count;
+
+    }
 };
