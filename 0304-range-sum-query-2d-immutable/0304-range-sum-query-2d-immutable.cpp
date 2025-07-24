ddc31bfcf1d187d79cb8370d12d367a1689723be
Isha Vashisht <120775114+Ishavashisht@users.noreply.github.com>
2025-07-24 18:18:43 +0530

    Time: 17 ms (83.68%), Space: 149.1 MB (85.19%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -5,25 +5,24 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         prefixSum.resize(m,vector<int>(n,0));
-     
-        for(int i=0;i<m;i++){
+       // prefixSum[0][0]=matrix[0][0];
+            for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                prefixSum[i][j]=matrix[i][j];
-                if(i>0)prefixSum[i][j]+=prefixSum[i-1][j];
-                if(j>0)prefixSum[i][j]+=prefixSum[i][j-1];
-                if(i>0 && j>0)prefixSum[i][j]-=prefixSum[i-1][j-1];
+        prefixSum[i][j]=matrix[i][j];
+        if(i>0)prefixSum[i][j]+=prefixSum[i-1][j];
+        if(j>0)prefixSum[i][j]+=prefixSum[i][j-1];
+        if(i>0 && j>0)prefixSum[i][j]-=prefixSum[i-1][j-1];
             }
         }
     }
-  //  prefix[i][j] = matrix[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
-
+    
     int sumRegion(int row1, int col1, int row2, int col2) {
-      int total = prefixSum[row2][col2];
-        if (row1 > 0) total -= prefixSum[row1-1][col2];
-        if (col1 > 0) total -= prefixSum[row2][col1-1];
-        if (row1 > 0 && col1 > 0) total += prefixSum[row1-1][col1-1];
- return total;
-    }
+        int totalSum=prefixSum[row2][col2];
+        if(row1>0)totalSum-=prefixSum[row1-1][col2];
+        if(col1>0)totalSum-=prefixSum[row2][col1-1];
+        if(row1>0 && col1>0)totalSum+=prefixSum[row1-1][col1-1];
+        return totalSum;
+            }
 };
 
 /**
